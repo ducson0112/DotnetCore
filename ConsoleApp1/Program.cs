83816382ee7b47bf16cb12032fa171c785ce39b8
ducson0112 <nds01122003@gmail.com>
2025-05-15 16:23:11 +0700

    Add array,method,compare,calc

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine($"{i}");
+                i++;
+            }
+        }
+    }
+}
